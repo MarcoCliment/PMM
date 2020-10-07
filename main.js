72f620f5122359ef1cef58c81d4bf341c82d35b5
Marco Climent <cem201409@gmail.com>
2020-10-07 23:09:26 +0200

    AE1-6

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-//5. Escribe el código necesario para calcular el factorial del número 15, en menos de 5
-//instrucciones.
-    var numero = 15, multi = 1; 
-	for (let i=1; i<=numero; i++)
-        multi = multi * i; 
-    console.log(multi); //EN SOLO 4 INSTRUCCIONES
+/*6. Escribe el código de una función que pida al usuario un número entero y dicho número
+se pase a otra función que devuelva una cadena de texto con “N es número par” o “N
+es número impar”, y se muestre dicha cadena de texto por consola.*/
+function pedirNumero(){
+    var numero = Number.parseInt(window.prompt("Dame un número entero")); //Intentamos convertir el valor que nos llega a número
+    if (!isNaN(numero)){ //He probado varias maneras de comprobar que sea un entero, esta solución comprueba que no sea NaN: NOT A NUMBER. Si le mandamos float, elimina los decimales
+        esPar(numero); //llamamos a la función para saber si es par
+    }else{
+        console.log("No es un número entero.");    
+    }    
+}
+
+function esPar(n){ //Función que valida si el número pasado es par o impar
+    if(n % 2 === 0){ //Si el resto de dividir entre dos un operador da igual a 0, significa que es par
+        console.log(n+" es número par");
+    }else{
+        console.log(n+" es número impar");
+    }
+}
+
+pedirNumero();
