d3ca8dd0cbcc89cb5a54b60536f9f1b86a79a77f
Marco Climent <cem201409@gmail.com>
2020-10-07 21:58:49 +0200

    AE1-2

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-console.log("Hola Mundo!");
-/*
-a. Lanzar un mensaje de alerta al usuario con el siguiente texto: “Soy el primer
-script”.*/
-alert('Soy el primer script');
-/*
-b. El mensaje se almacene en una variable llamada “alertMessage” y siga
-funcionando de la misma manera.
-*/
-var alertMessage = 'Soy el primer script';
-alert(alertMessage);
-/*
-c. El mensaje se almacene en una constante “ALERT_MESSAGE” a la se
-concatene el texto “y estoy funcionando sobre “, seguido de la versión del
-motor del navegador en uso.
-*/
-const ALERT_MESSAGE = 'Soy el primer script';
-alert(ALERT_MESSAGE + ' y estoy funcionando sobre ' + 
-navigator.appVersion);
+//2. Crea un array de elementos que contenga el nombre de los 12 meses del año y…
+var mesesAny = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+//a. Haz que se escriban por la consola del navegador en líneas consecutivas.
+for(let mes of mesesAny){
+    console.log(mes);
+}    
+//b. Haz que se escriban en la consola del navegador en formato de tabla
+console.table(mesesAny);
+
