7cdd684e8974d774392aaf7b23b5f24646c69c8b
Marco Climent <cem201409@gmail.com>
2020-10-07 22:41:43 +0200

    AE1-5

--- main.js
+++ main.js
@@ -1,31 +1,6 @@
-//4. Dado el siguiente snippet de código:
-var numero1 = 5;
-var numero2 = 8;
-/*if(...) {
-    console.log("numero1 no es mayor que numero2");
-}
-if(...) {
-    console.log("numero2 es positivo");
-}
-if(...) {
-    console.log("numero1 es negativo o distinto de cero");
-}
-if(...) {
-    console.log("Incrementar en 1 unidad el valor de numero 1 "+
-    "no lo hace mayor o igual que numero2");
-}*/
-
-//Haz que las afirmaciones en las cadenas de texto no digan ninguna mentira.
-if(!(numero1>=numero2)) {
-    console.log("numero1 no es mayor que numero2");
-}
-if(numero2>=0) {
-    console.log("numero2 es positivo");
-}
-if(numero1<0 || numero1!=0 ) {
-    console.log("numero1 es negativo o distinto de cero");
-}
-if(!(numero1+1>=numero2)) {
-    console.log("Incrementar en 1 unidad el valor de numero 1 "+
-    "no lo hace mayor o igual que numero2");
-}
+//5. Escribe el código necesario para calcular el factorial del número 15, en menos de 5
+//instrucciones.
+    var numero = 15, multi = 1; 
+	for (let i=1; i<=numero; i++)
+        multi = multi * i; 
+    console.log(multi); //EN SOLO 4 INSTRUCCIONES
